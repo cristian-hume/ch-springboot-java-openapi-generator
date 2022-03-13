6746171d5e9cc6edda02fb78102f6bd16b6f6982
Cristian Hume Henriquez <cristian.hume@outlook.com>
2022-03-12 22:33:23 -0300

    [feature/CREATE-PROJECT]: Create a Spring Boot Project (STABLE-VERSION)
    * Create Spring Boot project with Spring Initializr https://start.spring.io/
    
    Signed-off-by: Cristian Hume Henriquez <cristian.hume@outlook.com>

---
+++ src/test/java/ch/springboot/java/app/openapigenerator/OpenapiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.springboot.java.app.openapigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
